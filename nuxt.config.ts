fc583dcc41a5c780084f71ce4ee0f88b38a36905
baimamboukar <baimamboukar@gmail.com>
2024-08-27 02:34:15 +0200

    chore: gen web icons assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
+
   modules: [
     '@nuxt/content',
     '@nuxt/ui',
     '@nuxt/fonts'
   ],
+
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
+
   devtools: {
     enabled: true
   },
+
   typescript: {
     strict: false
-  }
+  },
+
+  compatibilityDate: '2024-07-26'
 })
